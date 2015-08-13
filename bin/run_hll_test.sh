eee163dbbaf4e283326aba2b5377306e62c76d75
Hemanth Yamijala <yhemanth@gmail.com>
2015-08-13 11:55:21 +0530

    Modified script to take data directory as input

--- bin/run_hll_test.sh
+++ bin/run_hll_test.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then
-  echo "Usage: $0 num_runs set_a_cardinality set_b_cardinality intersection_cardinality";
+  echo "Usage: $0 num_runs set_a_cardinality set_b_cardinality intersection_cardinality data_dir";
   exit -1;
 fi
 num_runs=$1
 card_a=$2
 card_b=$3
 card_a_n_b=$4
+data_dir=$5
 for i in `seq 1 $num_runs`
 do
   file_name_prefix=ids_$2_$3_$4_run$i;
-  python data-utils/generate_distinct_ids.py ${card_a} ${card_b} ${card_a_n_b} 1000000000 data ${file_name_prefix};
-  python sketches/sketches/redis_hll_intersects.py data/${file_name_prefix}_1 data/${file_name_prefix}_2;
+  python data-utils/generate_distinct_ids.py ${card_a} ${card_b} ${card_a_n_b} 1000000000 ${data_dir} ${file_name_prefix};
+  python sketches/sketches/redis_hll_intersects.py ${data_dir}/${file_name_prefix}_1 ${data_dir}/${file_name_prefix}_2;
 done
