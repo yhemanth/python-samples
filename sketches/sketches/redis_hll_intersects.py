b3e91a4ca054a3970a694332d33d04be1cbf4e3b
Hemanth Yamijala <yhemanth@gmail.com>
2015-08-20 21:27:11 +0530

    Refactored code so that the program flow is clearer

--- sketches/sketches/redis_hll_intersects.py
+++ sketches/sketches/redis_hll_intersects.py
@@ -6,61 +6,73 @@ import redis
 REDIS_PIPELINE_BATCH_SIZE = 10000
 
 
-class RedisHllIntersects:
-    def __init__(self, host='127.0.0.1', port=6379):
-        self.redis_client = redis.StrictRedis(host, port)
+def common_key_name(k1, k2):
+    return "temp" + ":".join([k1, k2])
 
-    def intersect_ids(self, ids_files):
-        common_elements, k1, k2, s1_count_actual, s2_count_actual = self._add_keys(ids_files)
-        self._intersection_counts(common_elements, k1, k2, s1_count_actual, s2_count_actual)
-        self._cleanup(k1, k2)
 
-    def _cleanup(self, k1, k2):
-        self.redis_client.delete(k1, k2, self._common_key_name(k1, k2))
+class IdSet:
+    def __init__(self, ids_file, redis_client):
+        self.redis_client = redis_client
+        self.hll_key_name = os.path.basename(ids_file)
+        self.ids = set()
+        map(lambda x: self.ids.add(x), open(ids_file, 'r').readlines())
 
-    def _intersection_counts(self, common_elements, k1, k2, s1_count_actual, s2_count_actual):
-        k1_k2 = self._common_key_name(k1, k2)
-        self.redis_client.pfmerge(k1_k2, k1, k2)
-        k1_count = self.redis_client.pfcount(k1)
-        k2_count = self.redis_client.pfcount(k2)
-        k1_k2_count = self.redis_client.pfcount(k1_k2)
-        counts_str = ",".join(map(lambda x: str(x),
-                                  [s1_count_actual, k1_count, s2_count_actual, k2_count, len(common_elements),
-                                   (k1_count + k2_count - k1_k2_count)]))
-        print ",".join([self._common_key_name(k1, k2),counts_str])
-
-    @staticmethod
-    def _common_key_name(k1, k2):
-        return "temp" + ":".join([k1, k2])
-
-    def _add_keys(self, ids_files):
-        k1 = os.path.basename(ids_files[0])
-        common_elements = self._add_ids(ids_files[0], k1)
-        s1_count_actual = len(common_elements)
-        k2 = os.path.basename(ids_files[1])
-        second_set = self._add_ids(ids_files[1], k2)
-        common_elements = second_set.intersection(common_elements)
-        return common_elements, k1, k2, s1_count_actual, len(second_set)
-
-    def _add_ids(self, ids_file, hll_key_name):
-        self.redis_client.delete(hll_key_name)
-        s = set()
-        map(lambda x: s.add(x), open(ids_file, 'r').readlines())
-        self._add_ids_to_hll(hll_key_name, s)
-        return s
-
-    def _add_ids_to_hll(self, key, hll_set):
+    def add_to_hll(self):
+        self.redis_client.delete(self.hll_key_name)
         pipeline = self.redis_client.pipeline()
         batch_size = REDIS_PIPELINE_BATCH_SIZE
-        for hll_id in hll_set:
-            pipeline.pfadd(key, hll_id)
+        for hll_id in self.ids:
+            pipeline.pfadd(self.hll_key_name, hll_id)
             batch_size -= 1
             if batch_size == 0:
                 pipeline.execute()
                 batch_size = REDIS_PIPELINE_BATCH_SIZE
         pipeline.execute()
 
+    def actual_count(self):
+        return len(self.ids)
+
+    def hll_count(self):
+        return self.redis_client.pfcount(self.hll_key_name)
+
+    def intersection_count(self, other_id_set):
+        return len(self.ids.intersection(other_id_set.ids))
+
+    def intersect_hlls_using_inclusion_exclusion(self, other_id_set):
+        merged_key_name = common_key_name(self.hll_key_name, other_id_set.hll_key_name)
+        self.redis_client.pfmerge(merged_key_name, self.hll_key_name, other_id_set.hll_key_name)
+        intersection_count = self.hll_count() + other_id_set.hll_count() - self.redis_client.pfcount(merged_key_name)
+        return merged_key_name, intersection_count
+
+    def cleanup(self):
+        self.redis_client.delete(self.hll_key_name)
+
+
+class RedisHllIntersects:
+    def __init__(self, host='127.0.0.1', port=6379):
+        self.redis_client = redis.StrictRedis(host, port)
+
+    def intersect_ids(self, ids_file1, ids_file2):
+        id_set1 = IdSet(ids_file1, self.redis_client)
+        id_set1.add_to_hll()
+
+        id_set2 = IdSet(ids_file2, self.redis_client)
+        id_set2.add_to_hll()
+
+        merged_key_name, hll_intersection_count = id_set1.intersect_hlls_using_inclusion_exclusion(id_set2)
+
+        counts_str = ",".join(map(lambda x: str(x),
+                                  [id_set1.actual_count(), id_set1.hll_count(),
+                                   id_set2.actual_count(), id_set2.hll_count(),
+                                   id_set1.intersection_count(id_set2),
+                                   hll_intersection_count]))
+        print ",".join([merged_key_name, counts_str])
+
+        id_set1.cleanup()
+        id_set2.cleanup()
+        self.redis_client.delete(merged_key_name)
+
 
 if __name__ == "__main__":
     redisHllTester = RedisHllIntersects()
-    redisHllTester.intersect_ids([sys.argv[1], sys.argv[2]])
+    redisHllTester.intersect_ids(sys.argv[1], sys.argv[2])
